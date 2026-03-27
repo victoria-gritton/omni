5c39beda5efab95644bb1cda803fcc6bb0c3cb27
victoria gritton <grittv@amazon.com>
2026-03-26 19:53:51 -0700

    Switch to HashRouter for static hosting compatibility

--- demo/src/main.jsx
+++ demo/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import WatchView from './pages/WatchView'
 import PhoneView from './pages/PhoneView'
 import ConsoleView from './pages/ConsoleView'
@@ -11,7 +11,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/watch" element={<WatchView />} />
@@ -19,6 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/home" element={<ConsoleLayout><HomePage /></ConsoleLayout>} />
         <Route path="/console" element={<ConsoleLayout><ConsoleView /></ConsoleLayout>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
